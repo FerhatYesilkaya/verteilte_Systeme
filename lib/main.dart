244bd6042c21fe93ea574625eb9326d811280004
Ferhat Yesilkaya <ferhat-20001@hotmail.de>
2022-05-15 17:21:36 +0200

    Neue Daten

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:shopper/storage_service.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -11,7 +18,6 @@ void main() {
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -19,8 +25,68 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final Storage storage = Storage();
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text('Shopify'),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: ElevatedButton(
+                onPressed: () async {
+                  final results = await FilePicker.platform.pickFiles(
+                    allowMultiple: false,
+                    type: FileType.custom,
+                    allowedExtensions: ['png','jpg'],
+                  );
+
+                  if(results == null) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text("No File Selected"),
+                    ),
+                    );
+                    return null;
+                  }
+
+                  final path = results.files.single.path!;
+                  final fileName = results.files.single.name;
+
+                  storage.uploadFile(path, fileName).then((value) => print("Done"));
+                },
+                child: Text("Upload File")),
+          ),
+
+          FutureBuilder(
+            future: storage.listFiles(),
+            builder: (BuildContext context,
+                AsyncSnapshot<firebase_storage.ListResult> snapshot) {
+              if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.hasData) {
+                return Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  height: 50,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.items.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ElevatedButton(
+                          onPressed: () {},
+                          child: Text(snapshot.data!.items[index].name),
+                        );
+                      }),
+                );
+              }
+              if(snapshot.connectionState == ConnectionState.waiting ||
+              snapshot.hasData) {
+                return CircularProgressIndicator();
+              }
+              return Container();
+            }),
+        ],
+      ),
     );
   }
 }
